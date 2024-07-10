992bfd1d59c694147ba6e831588f839be601b5bf
mtzkmy <162808962+mtzkmy@users.noreply.github.com>
2024-07-10 23:31:45 +0900

    Add files via upload

--- color/js/color.js
+++ color/js/color.js
@@ -1,5 +1,13 @@
-// カラーコードを入手する
-console.log(document.querySelector('#ColorPicker').value);
-// カラーコードに変える
-document.querySelector('#colorText').textContent = 'カラーコード:';
-sd
+
+// // カラーコードを表示するように値を取得
+// document.querySelector('#colorText').textContent = `カラーコード:${document.querySelector('#ColorPicker').value}`;
+
+// でも長すぎるので定数宣言
+const text = document.querySelector('#colorText');
+
+const picker = document.querySelector('#ColorPicker');
+
+text.textContent = `カラーコード: ${picker.value}` ;
+
+// 色が選択されたらカラーコードを表示する
+picker.addEventListener('input', colorBg);
